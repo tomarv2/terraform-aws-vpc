ea38fbb4df332fd9e0eed59bbcf65cf803dc76f7
tomarv2 <varuntomar2019@gmail.com>
2021-05-04 08:53:19 -0700

    moving to pre-commit

--- locals.tf
+++ locals.tf
@@ -1,7 +1,9 @@
 locals {
-  shared_tags = map(
-    "Name", "${var.teamid}-${var.prjid}",
-    "team", var.teamid,
-    "project", var.prjid
+  shared_tags = tomap(
+    {
+      "Name"    = "${var.teamid}-${var.prjid}",
+      "team"    = var.teamid,
+      "project" = var.prjid
+    }
   )
 }
